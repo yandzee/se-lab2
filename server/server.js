b664a6d816f8dd85ebe7ec02bd4834c6e8ca4502
Paul Loyd <pavelko95@gmail.com>
2016-04-28 15:52:59 +0300

    feat(server): searching

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ class Server {
     app.context.searcher = searcher;
 
     app.use(this.logger);
+    app.use(this.period);
+    app.use(this.revol);
     app.use(this.file);
   }
 
@@ -27,6 +29,50 @@ class Server {
     yield* next;
     let ms = Date.now() - start;
     console.log('%s %s - %sms', this.method, this.url, ms);
+  }
+
+  *period(next) {
+    if (this.path !== '/period')
+      return yield* next;
+
+    let asNum = str => +(str || NaN);
+    let satnum = asNum(this.query.satnum);
+    let ts0 = asNum(this.query.ts0);
+    let ts1 = asNum(this.query.ts1) || Date.now();
+
+    let allAreSafe = [satnum, ts0, ts1].every(Number.isSafeInteger);
+    if (!allAreSafe || satnum < 0) {
+      this.status = 422;
+      return;
+    }
+
+    let result = yield* this.searcher.period(satnum, ts0, ts1);
+    console.log(result);
+
+    this.type = 'json';
+    this.body = result;
+  }
+
+  *revol(next) {
+    if (this.path !== '/revol')
+      return yield* next;
+
+    let asNum = str => +(str || NaN);
+    let satnum = asNum(this.query.satnum);
+    let ts = asNum(this.query.ts) || Date.now();
+    let nrevs = asNum(this.query.nrevs) || 1;
+
+    let allAreSafe = [satnum, ts, nrevs].every(Number.isSafeInteger);
+    if (!allAreSafe || satnum < 0 || nrevs < 1) {
+      this.status = 422;
+      return;
+    }
+
+    let result = yield* this.searcher.revolutions(satnum, ts, nrevs);
+    console.log(result);
+
+    this.type = 'json';
+    this.body = result;
   }
 
   *file(next) {
