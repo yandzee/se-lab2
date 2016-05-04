60b9c0a2bbcfe1251c958a09895c9c95aebaf6a4
Renat <yandzeek@gmail.com>
2016-05-04 23:23:17 +0300

    style(client): fix style

--- client/info.js
+++ client/info.js
@@ -28,20 +28,21 @@ class InfoComponent extends EventEmitter {
   }
 
   displayInfo(satnum) {
+    let nullchk = (v, fv) => v == null ? '-' : (fv == null ? v : fv);
     fetcher.fetchInfo(satnum).then(info => {
       info.satnum = satnum;
-      info.intl = info.intl || '-';
-      info.perigee = info.perigee || '-';
-      info.apogee = info.apogee || '-';
-      info.inclincation = info.inclincation || '-';
-      info.period = info.period || '-';
-      info.semimajor = info.semimajor || '-';
-      info.rcs = info.rcs || '-';
-      info.launch = (new Date(info.launch)) || '-';
-      info.decay = info.decay || '-';
-      info.source = info.source || '-';
-      info.site = info.site || '-';
-      info.note = info.note || '-';
+      info.intl = nullchk(info.intl);
+      info.perigee = nullchk(info.perigee);
+      info.apogee = nullchk(info.apogee);
+      info.inclincation = nullchk(info.inclincation);
+      info.period = nullchk(info.period);
+      info.semimajor = nullchk(info.semimajor);
+      info.rcs = nullchk(info.rcs);
+      info.launch = nullchk(info.launch, new Date(info.launch));
+      info.decay = nullchk(info.decay, new Date(info.decay));
+      info.source = nullchk(info.source);
+      info.site = nullchk(info.site);
+      info.note = nullchk(info.note);
 
       let html = tmpl('sat-info-template');
       this.infoDiv.html($(html(info)));
