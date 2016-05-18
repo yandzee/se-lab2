878618b02b5849a5b821322fbb78d82e5fd57c5e
Renat <yandzeek@gmail.com>
2016-05-18 23:25:20 +0300

    chore(client): oops, restored deleted file with hole logic

---
+++ client/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let list = new ListComponent($('#list-component'));
+let map  = new MapComponent($('#map-component'));
+let info = new InfoComponent($('#info-component'));
+
+list.on('selected', satnum => {
+  map.showSatellite(satnum);
+  info.showInfo(satnum);
+});
+
+list.on('unselected', satnum => {
+  map.hideSatellite(satnum);
+  info.showLast(satnum);
+});
+
+map.on('error', _ => {
+  $('#error-modal').modal();
+});
